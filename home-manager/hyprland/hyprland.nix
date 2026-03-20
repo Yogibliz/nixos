c50b6c5a8573aed2e58ea0ce405945ac21e21fe9
Iris <irisphere@pm.me>
2026-03-20 13:13:53 +0100

    Add all hyprland monitor setups used.

--- home-manager/hyprland/hyprland.nix
+++ home-manager/hyprland/hyprland.nix
@@ -17,10 +17,18 @@
     settings = {
       # Monitor config
       monitor = [
-        "DP-2, 1920x1080@60, 0x0, 1, transform, 3"
-        "DP-1, 3440x1440@175, 1080x0, 1"
-        "DP-3, 1920x1080@144, 4520x0, 1"
-        "HDMI-A-1, 3840x2160@120, 6440x0, 1.5"
+        # School
+        "DP-2, 2540x1440@60, 2540x0, 1"
+        "DP-1, 2540x1440@60, auto, 1"
+
+        # Home
+        # "DP-2, 1920x1080@60, 0x0, 1, transform, 3"
+        # "DP-1, 3440x1440@175, 1080x, 1"
+        # "DP-3, 1920x1080@144, 4520x0, 1"
+        # "HDMI-A-1, 3840x2160@120, 6440x0, 1.5"
+
+        # Laptop
+        # ",preferred, auto, 1.5"
       ];
 
       # Window Rules
@@ -64,6 +72,7 @@
 
       exec-once = [
         "ashell"
+        "hyprshell -run"
       ];
 
       input = {
