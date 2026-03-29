e14b85e3dcb074d8c1f24a095d8ce90ea8e16c07
Iris <irisphere@pm.me>
2026-03-30 01:13:48 +0200

    working on getting laptop audio working

--- nixos/hosts/laptop/configuration.nix
+++ nixos/hosts/laptop/configuration.nix
@@ -1,5 +1,19 @@
-{...}: {
+{pkgs, ...}: {
   networking.hostName = "laptop";
 
   # Might have to add kernel rebuild for audio fix...
+  boot.kernelPatches = [
+    {
+      name = "samsung-galaxybook4-audio";
+      patch = pkgs.fetchurl {
+        url = "https://patch-diff.githubusercontent.com/raw/thesofproject/linux/pull/5616.patch";
+        hash = "sha256-Ip6O4les1Hi2Rwrwi9aKhLkAmEJiAbmPXdFrAyHhCZo=";
+      };
+      extraConfig = ''
+        SND_HDA_SCODEC_MAX98390 m
+        SND_HDA_SCODEC_MAX98390_I2C m
+        SND_SOC_MAX98390 m
+      '';
+    }
+  ];
 }
