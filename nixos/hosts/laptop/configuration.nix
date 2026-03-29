45f75d808a8da0b907effe9d90b7788cddd40029
Iris <irisphere@pm.me>
2026-03-30 01:17:22 +0200

    working on getting laptop audio working

--- nixos/hosts/laptop/configuration.nix
+++ nixos/hosts/laptop/configuration.nix
@@ -5,10 +5,7 @@
   boot.kernelPatches = [
     {
       name = "samsung-galaxybook4-audio";
-      patch = pkgs.fetchurl {
-        url = "https://patch-diff.githubusercontent.com/raw/thesofproject/linux/pull/5616.patch";
-        hash = "sha256-Ip6O4les1Hi2Rwrwi9aKhLkAmEJiAbmPXdFrAyHhCZo=";
-      };
+      patch = ./samsung-audio.patch;
       extraConfig = ''
         SND_HDA_SCODEC_MAX98390 m
         SND_HDA_SCODEC_MAX98390_I2C m
@@ -16,4 +13,8 @@
       '';
     }
   ];
+
+  boot.extraModprobeConfig = ''
+    options snd-hda-intel model=alc298-samsung-max98390-4-amps
+  '';
 }
