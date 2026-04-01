976b3e825bbce0e07d992b46f4eb573b5aa1a8f7
Iris <irisphere@pm.me>
2026-04-01 22:00:09 +0200

    fuck hyprshell

--- home-manager/modules/hyprland/hyprshell.nix
+++ home-manager/modules/hyprland/hyprshell.nix
@@ -5,21 +5,25 @@
       windows = {
         scale = 8.0;
         overview = {
-          enable = true;
           key = "Super_L";
           modifier = "super";
           launcher = {
             max_items = 6;
             default_terminal = "ghostty";
             plugins = {
-              applications.enable = true;
-              calc.enable = true;
-              terminal.enable = true;
+              applications = {
+                enable = true;
+              };
+              calc = {
+                enable = true;
+              };
+              terminal = {
+                enable = true;
+              };
             };
           };
         };
         switch = {
-          enable = true;
           key = "Tab";
           modifier = "alt";
           filter_by = ["current_workspace"];
--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -10,7 +10,6 @@
       ll = "eza -la";
       la = "eza -a";
       cat = "bat";
-      hms = "home-manager switch --flake ~/dotfiles#$(hostname)";
       nrs = "sudo nixos-rebuild switch --flake ~/dotfiles#$(hostname)";
       nrb = "sudo nixos-rebuild boot --flake ~/dotfiles#$(hostname)";
       time = "command time -f '======= Time & Memory =======\n\t%e seconds, \n\t%S systime (kernel), \n\t%P CPU, \n\t%M KB memory (MAX)'";
