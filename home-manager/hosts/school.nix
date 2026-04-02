a79009c1278d9a4af60d97a32a3944d07ff56e00
Iris <irisphere@pm.me>
2026-04-02 10:13:41 +0200

    school allow pkgs

--- home-manager/hosts/school.nix
+++ home-manager/hosts/school.nix
@@ -1,6 +1,9 @@
-{...}: {
+{pkgs}: {
   wayland.windowManager.hyprland.settings.monitor = [
     "DP-1, 2540x1440@60, 2540x0, 1"
     "DP-2, 2540x1440@60, 0x0, 1"
+  ];
+
+  home.packages = with pkgs; [
   ];
 }
