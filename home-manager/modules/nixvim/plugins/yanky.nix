36496cdc5d69844b52095a947724bae24be8e7f7
Iris <irisphere@pm.me>
2026-04-04 23:03:29 +0200

    yanky, not working

--- home-manager/modules/nixvim/plugins/yanky.nix
+++ home-manager/modules/nixvim/plugins/yanky.nix
@@ -3,33 +3,36 @@
   programs.nixvim.plugins.yanky = {
     enable = true;
     enableTelescope = true;
+
     settings = {
       highlight = {
         on_put = true;
         on_yank = true;
         timer = 500;
       };
+
       picker = {
         telescope = {
           mappings = {
             default = "mapping.put('p')";
             i = {
-              "<c-g>" = "mapping.put('p')";
-              "<c-k>" = "mapping.put('P')";
-              "<c-r>" = "mapping.set_register(utils.get_default_register())";
-              "<c-x>" = "mapping.delete()";
+              "<c-g>".__raw = "mapping.put('p')";
+              "<c-k>".__raw = "mapping.put('P')";
+              "<c-r>".__raw = "mapping.set_register(utils.get_default_register())";
+              "<c-x>".__raw = "mapping.delete()";
             };
             n = {
-              P = "mapping.put('P')";
-              d = "mapping.delete()";
-              gP = "mapping.put('gP')";
-              gp = "mapping.put('gp')";
-              p = "mapping.put('p')";
-              r = "mapping.set_register(utils.get_default_register())";
+              P.__raw = "mapping.put('P')";
+              d.__raw = "mapping.delete()";
+              gP.__raw = "mapping.put('gP')";
+              gp.__raw = "mapping.put('gp')";
+              p.__raw = "mapping.put('p')";
+              r.__raw = "mapping.set_register(utils.get_default_register())";
             };
           };
         };
       };
+
       preserve_cursor_position = {
         enabled = true;
       };
