0387cf9941e77cdaeba4611601330b704703b235
Iris <irisphere@pm.me>
2026-04-04 18:32:23 +0200

    testing nixvim

--- home-manager/modules/nixvim/plugins/blink.nix
+++ home-manager/modules/nixvim/plugins/blink.nix
@@ -2,7 +2,11 @@
   programs.nixvim.plugins.blink-cmp = {
     enable = true;
     settings = {
-      
+      keymap = {
+	preset = "default";
+	"<Tab>" = [ "accept" "fallback" ];
+	"<CR>" = [ "fallback" ];
+      };
     };
   };
 }
