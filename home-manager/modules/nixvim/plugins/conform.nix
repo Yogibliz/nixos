2995352354c5dae37a877419e11634471a19a902
Iris <irisphere@pm.me>
2026-04-04 23:48:33 +0200

    added autopairs

--- home-manager/modules/nixvim/plugins/conform.nix
+++ home-manager/modules/nixvim/plugins/conform.nix
@@ -8,7 +8,7 @@
         lsp_format = "fallback";
       };
       formatters_by_ft = {
-        nix = [ "alejandra" ];
+        nix = [ "nixfmt" ];
         lua = [ "stylua" ];
         rust = [ "rustfmt" ];
         python = [ "black" ];
@@ -18,7 +18,7 @@
   };
 
   programs.nixvim.extraPackages = with pkgs; [
-    alejandra
+    nixfmt
     stylua
     rustfmt
   ];
