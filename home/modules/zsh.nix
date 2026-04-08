adf0520db71c39eb4a007f997171776c5ef7557a
Iris <irisphere@pm.me>
2026-04-08 15:06:00 +0200

    new alias

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -13,6 +13,7 @@
       cat = "bat";
       nrs = "sudo nixos-rebuild switch --flake ~/dotfiles#$(hostname)";
       nrb = "sudo nixos-rebuild boot --flake ~/dotfiles#$(hostname)";
+      noct = "nix run nixpkgs#noctalia-shell ipc call state all > ./modules/features/noctalia.json";
       time = "command time -f '======= Time & Memory =======\n\t%e seconds, \n\t%S systime (kernel), \n\t%P CPU, \n\t%M KB memory (MAX)'";
       pi = "ssh -p 2222 dietpi@192.168.1.100";
     };
