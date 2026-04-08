ea71588bb1e3fa60eb3fdcfbac13d5851d1a9eaa
Iris <irisphere@pm.me>
2026-04-08 14:00:18 +0200

    add inputs and self to laptop and school default

--- modules/hosts/laptop/default.nix
+++ modules/hosts/laptop/default.nix
@@ -1,6 +1,8 @@
 { self, inputs, ... }:
 {
   flake.nixosConfigurations.laptop = inputs.nixpkgs.lib.nixosSystem {
+    specialArgs = { inherit inputs self; };
+
     modules = [
       self.nixosModules.laptopConfiguration
     ];
--- modules/hosts/school/default.nix
+++ modules/hosts/school/default.nix
@@ -1,6 +1,8 @@
 { self, inputs, ... }:
 {
   flake.nixosConfigurations.school = inputs.nixpkgs.lib.nixosSystem {
+    specialArgs = { inherit inputs self; };
+
     modules = [
       self.nixosModules.schoolConfiguration
     ];
