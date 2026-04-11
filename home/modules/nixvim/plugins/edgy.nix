f8920dfc36d4b31de701270d803fd16a5a37c977
Iris <irisphere@pm.me>
2026-04-11 03:35:39 +0200

    testing

--- home/modules/nixvim/plugins/edgy.nix
+++ home/modules/nixvim/plugins/edgy.nix
@@ -15,6 +15,11 @@
           ft = "toggleterm";
           size = 20;
           title = "Terminal";
+          filter.__raw = ''
+            function(buf, win)
+              return vim.api.nvim_win_get_config(win).relative == ""
+            end
+          '';
         }
       ];
     };
