3f05d5a2d758e07a5c5755c6dd4b60a807f8c1e7
Iris <irisphere@pm.me>
2026-04-11 03:26:21 +0200

    testing

--- home/modules/nixvim/plugins/toggleterm.nix
+++ home/modules/nixvim/plugins/toggleterm.nix
@@ -2,5 +2,12 @@
 {
   programs.nixvim.plugins.toggleterm = {
     enable = true;
+    settings = {
+      open_mapping = "[[<C-t>]]";
+      direction = "float";
+      float_opts = {
+        border = "curved";
+      };
+    };
   };
 }
