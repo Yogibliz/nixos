da9d0eadb4fbd6536dc42f91b24b9e30b4423139
Iris <irisphere@pm.me>
2026-04-17 23:30:25 +0200

    remove desktop disks from laptop and school config

--- modules/hosts/school/configuration.nix
+++ modules/hosts/school/configuration.nix
@@ -38,22 +38,5 @@
 
       networking.hostName = "school";
 
-      fileSystems."/mnt/games" = {
-        device = "/dev/disk/by-uuid/c82bd535-419a-4d80-92c1-22f3c4b88eb4";
-        fsType = "ext4";
-        options = [
-          "defaults"
-          "nofail"
-        ];
-      };
-
-      fileSystems."/mnt/media" = {
-        device = "/dev/disk/by-uuid/4c4cf374-ebe1-4d9e-8c42-e1b0fdc5d764";
-        fsType = "ext4";
-        options = [
-          "defaults"
-          "nofail"
-        ];
-      };
     };
 }
