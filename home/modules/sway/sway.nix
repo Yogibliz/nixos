abffdf1421a7c0af6ad6b5dfec291ea8cc6f7eed
Iris <irisphere@pm.me>
2026-05-04 16:35:04 +0200

    ricing WiP

--- home/modules/sway/sway.nix
+++ home/modules/sway/sway.nix
@@ -1,9 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 {
   wayland.windowManager.sway = {
     enable = true;
+    package = pkgs.swayfx;
     wrapperFeatures.gtk = true;
 
+    checkConfig = false;
+
+    # Base Sway config here.
     config = {
       modifier = "Mod4";
       terminal = "ghostty";
@@ -25,22 +29,38 @@
         { command = "vicinae server"; }
       ];
 
-      window.border = 2;
-      window.commands = [
-        {
-          command = "floating enable";
-          criteria = {
-            app_id = "^steam$";
-          };
-        }
-        {
-          command = "floating disable";
-          criteria = {
-            app_id = "^steam$";
-            title = "^Steam$";
-          };
-        }
-      ];
+      window = {
+        border = 0;
+        titlebar = false;
+        commands = [
+          {
+            command = "floating enable";
+            criteria = {
+              app_id = "^steam$";
+            };
+          }
+          {
+            command = "floating disable";
+            criteria = {
+              app_id = "^steam$";
+              title = "^Steam$";
+            };
+          }
+        ];
+      };
     };
+
+    # Swayfx config here to seperate.
+    extraConfig = ''
+      corner_radius 10
+
+      blur enable
+      blur_passes 2
+      blur_radius 5
+
+      shadows enable
+      shadow_blur_radius 15
+      shadow_color #0000007F
+    '';
   };
 }
