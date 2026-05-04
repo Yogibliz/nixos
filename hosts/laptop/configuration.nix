51e284ba3cb8d5e386c6c5458a7d74519d32455c
Iris <irisphere@pm.me>
2026-05-05 01:53:00 +0200

    updated config files, removed flake.nixosModules in favor of self, inputs etc.

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,50 +1,46 @@
-{ self, inputs, ... }:
 {
-  flake.nixosModules.laptopConfiguration =
-    {
-      pkgs,
-      lib,
-      hostname,
-      ...
-    }:
-    {
-      imports = [
-        self.nixosModules.laptopHardware
+  self,
+  inputs,
+  hostname,
+  pkgs,
+  lib,
+  ...
+}:
 
-        # shared NixOS modules
-        self.nixosModules.boot
-        self.nixosModules.swap
-        self.nixosModules.environment
-        self.nixosModules.fonts
-        self.nixosModules.sharedHardware
-        self.nixosModules.i18n
-        self.nixosModules.networking
-        self.nixosModules.nix
-        self.nixosModules.security
-        self.nixosModules.services
-        self.nixosModules.system
-        self.nixosModules.time
-        self.nixosModules.users
+{
+  imports = [
+    ./hardware.nix
+    # shared NixOS modules via self
+    self.nixosModules.boot
+    self.nixosModules.swap
+    self.nixosModules.environment
+    self.nixosModules.fonts
+    self.nixosModules.sharedHardware
+    self.nixosModules.i18n
+    self.nixosModules.networking
+    self.nixosModules.nix
+    self.nixosModules.security
+    self.nixosModules.services
+    self.nixosModules.system
+    self.nixosModules.time
+    self.nixosModules.users
+    # features
+    self.nixosModules.hyprland
+    self.nixosModules.noctalia
+    self.nixosModules.homeManager
+    self.nixosModules.samsungAudio
+    self.nixosModules.proton
+  ];
 
-        # features
-        self.nixosModules.hyprland
-        self.nixosModules.noctalia
-        self.nixosModules.homeManager
-        self.nixosModules.samsungAudio
-        self.nixosModules.proton
-      ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
-      nix.settings.experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
+  home-manager.users.iris.imports = [
+    "${self}/home/home.nix"
+    "${self}/home/hosts/laptop.nix"
+  ];
 
-      home-manager.users.iris.imports = [
-        "${self}/home/home.nix"
-        "${self}/home/hosts/laptop.nix"
-      ];
-
-      networking.hostName = hostname;
-
-    };
+  networking.hostName = hostname;
 }
--- hosts/school/configuration.nix
+++ hosts/school/configuration.nix
@@ -1,49 +1,45 @@
-{ self, inputs, ... }:
 {
-  flake.nixosModules.schoolConfiguration =
-    {
-      pkgs,
-      lib,
-      hostname,
-      ...
-    }:
-    {
-      imports = [
-        self.nixosModules.schoolHardware
+  self,
+  inputs,
+  hostname,
+  pkgs,
+  lib,
+  ...
+}:
 
-        # shared NixOS modules
-        self.nixosModules.boot
-        self.nixosModules.swap
-        self.nixosModules.environment
-        self.nixosModules.fonts
-        self.nixosModules.sharedHardware
-        self.nixosModules.i18n
-        self.nixosModules.networking
-        self.nixosModules.nix
-        self.nixosModules.security
-        self.nixosModules.services
-        self.nixosModules.system
-        self.nixosModules.time
-        self.nixosModules.users
+{
+  imports = [
+    ./hardware.nix
+    # shared NixOS modules via self
+    self.nixosModules.boot
+    self.nixosModules.swap
+    self.nixosModules.environment
+    self.nixosModules.fonts
+    self.nixosModules.sharedHardware
+    self.nixosModules.i18n
+    self.nixosModules.networking
+    self.nixosModules.nix
+    self.nixosModules.security
+    self.nixosModules.services
+    self.nixosModules.system
+    self.nixosModules.time
+    self.nixosModules.users
+    # features
+    self.nixosModules.hyprland
+    self.nixosModules.noctalia
+    self.nixosModules.homeManager
+    self.nixosModules.proton
+  ];
 
-        # features
-        self.nixosModules.hyprland
-        self.nixosModules.noctalia
-        self.nixosModules.homeManager
-        self.nixosModules.proton
-      ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
-      nix.settings.experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
+  home-manager.users.iris.imports = [
+    "${self}/home/home.nix"
+    "${self}/home/hosts/school.nix"
+  ];
 
-      home-manager.users.iris.imports = [
-        "${self}/home/home.nix"
-        "${self}/home/hosts/school.nix"
-      ];
-
-      networking.hostName = hostname;
-
-    };
+  networking.hostName = hostname;
 }
