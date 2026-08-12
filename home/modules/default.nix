6d6c7e08842f88c82445f7ab9a59b7201dbdc3ed
Iris <irisphere@pm.me>
2026-08-12 16:57:48 +0200

    top-level default.nix imports without me having to remember to add modules here now, yay. I still gotta remember to exclude them, but should be less work

--- home/modules/default.nix
+++ home/modules/default.nix
@@ -1,13 +1,20 @@
+{ lib, ... }:
+
 {
-  imports = [
-    ./ghostty.nix
-    ./git.nix
-    ./hyprland
-    ./nixvim
-    ./noctalia.nix
-    ./obs.nix
-    ./vicinae.nix
-    ./zen.nix
-    ./zsh.nix
-  ];
+  # I hate Lamda style functions, they're unreadable...
+  # To summarise this reads ./. for all files and directories
+  # then filters the readDir results ex { "hyprland" = "directory"; "ghostty.nix" = "regular" }
+  # accepting it as long as it's not named default.nix or artix-launcher
+  # and it's either a directory or .nix file. This ensures no recursive importing of default.nix
+  # and sub folders can handle their own imports through their own default.nix files.
+  imports = lib.mapAttrsToList (file: _: ./. + "/${file}") (
+    lib.filterAttrs (
+      name: type:
+      !(builtins.elem name [
+        "default.nix"
+        "artix-launcher"
+      ])
+      && (type == "directory" || lib.hasSuffix ".nix" name)
+    ) (builtins.readDir ./.)
+  );
 }
